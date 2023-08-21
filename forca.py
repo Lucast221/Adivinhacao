92660230cabeba413022819190c16c67ae1e4b1a
lucas <teixeiralucas940@outlook.com>
2023-08-21 10:42:30 -0300

    Correções e aperfeiçoamentos
    Tratamento adequado; contadores; retorno das letras acertadas e condição para vitória e derrota.

--- forca.py
+++ forca.py
@@ -4,24 +4,42 @@ def jogar():
     print("***Bem-vindo ao jogo da Forca \(0.0)/***")
     print("****************************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()  # Tratamento de entrada
+    qtd_ = len(palavra_secreta)
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     # Definição da boleana para o laço
     enforcou = False
     acertou = False
+    erros = 0
 
-    while (not enforcou and not acertou):
-        # Tratamento da entrada para ser recebida em minúsculas e sem espaços
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):  # Enquanto não for enforcado faça:
         chute = input("Chute uma letra: ")
-        chute = chute.strip()
-        chute = chute.lower()
+        chute = chute.strip().upper()  # Tratamento de entrada
 
-        index = 1
-        # Laço com tratamento de entrada
-        for letra in palavra_secreta:
-            if (chute.lower() == letra.lower()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
-        print("Jogando...")
+        # Se houver na palavra a letra do chute, vai executar o próximo comando
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:  # Para a letra acertada:
+                if (chute == letra):
+                    # Subistituirá "_" pela letra acertada na posição do index
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1  # Caso não acerte, somará um erro
+
+        enforcou = erros == qtd_  # Ao atingir o limite de erro, será enforcado
+        # Ao preencher todas as lacunas "_", ganha
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+        chances = qtd_ - erros
+        print("Você possui {} chances...".format(chances))
+
+    if (acertou):  # Resultado
+        print("Parabéns, você conseguiu!!")
+    else:  # Resultado
+        print("Você foi enforcado :´(")
 
     print("Fim do jogo")
 
